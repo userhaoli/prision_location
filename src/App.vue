50a004e532c6c10e7443507286936fa3103093f2
lihao <1911077369@qq.com>
2020-06-02 15:12:06 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    this.computedRem();
+    window.addEventListener("resize",()=>{
+      this.computedRem();
+    })
+  },
+  methods: {
+    computedRem() {
+      var docEl = document.documentElement;
+      if (docEl.clientWidth > 1200) {
+        docEl.style.fontSize = "10px";
+      } else {
+        docEl.style.fontSize = "8px";
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less">
+</style>
